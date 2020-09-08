9aa18fef2bfb7153be1b8e633ef15bf83db396c8
Jacob Levitin <jlevitin92@gmail.com>
2020-09-08 14:06:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   counter = 0
   board.each do |move|
-    if move == "O" or "X"
+    if move == "O"
+      counter += 1
+    elsif move == "X"
       counter += 1
     end
   end
